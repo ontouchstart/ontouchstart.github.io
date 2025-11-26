b25fc5902ef1ea8635fdbf49811f40308ee5a536
Sam Liu <ontouchstart@gmail.com>
2025-11-25 19:11:32 -0500

    what-is-ChatGPT-chinese-generate.json

--- 2025/11/25/FastAPI/Qwen/Qwen3-30B-A3B-MLX-4bit/main.py
+++ 2025/11/25/FastAPI/Qwen/Qwen3-30B-A3B-MLX-4bit/main.py
@@ -21,13 +21,18 @@ async def get_tokens(prompt: str):
 
 @app.get("/generate")
 async def get_generate(prompt: str):
+    raw_tokens = tokenizer.encode(prompt)
     conversation = [{"role": "user", "content": prompt}]
     prompt_tokens = tokenizer.apply_chat_template(conversation=conversation, add_generation_prompt=True)
     result = generate(model=model, tokenizer=tokenizer, prompt=prompt_tokens, max_tokens=1024)
+    raw_result = generate(model=model, tokenizer=tokenizer, prompt=raw_tokens, max_tokens=1024)
 
     return {
+            "raw_result_tokens": [{ token: tokenizer.decode(token)} for token in tokenizer.encode(raw_result)],
             "conversation" : conversation,
             "prompt_tokens" : [{ token: tokenizer.decode(token)} for token in prompt_tokens],
-            "result_tokens": [{ token: tokenizer.decode(token)} for token in tokenizer.encode(result)]
+            "result_tokens": [{ token: tokenizer.decode(token)} for token in tokenizer.encode(result)],
+            "raw_result": raw_result,
+            "result": result
             }
 
