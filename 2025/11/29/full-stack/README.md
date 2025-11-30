4b5f374ef842dce5bc81b1a0cae7e450b303a922
Sam Liu <ontouchstart@gmail.com>
2025-11-29 21:40:39 -0500

    https://ontouchstart.github.io/2025/11/29/full-stack

---
+++ 2025/11/29/full-stack/README.md
@@ -0,0 +1,10 @@
+# 2025/11/29/full-stack
+
+![screenshot-tokens-request.png](screenshot-tokens-request.png)
+![screenshot-tokens-response.png](screenshot-tokens-response.png)
+
+
+![screenshot-generate-request.png](screenshot-generate-request.png)
+![screenshot-generate-response.png](screenshot-generate-response.png)
+
+
---
+++ 2025/11/29/full-stack/test.py
@@ -0,0 +1,30 @@
+from playwright.sync_api import Page, expect
+
+url = "http://127.0.0.1:8000/docs"
+
+prompt = "What is Fast API ?"
+timeout = 5 * 60 * 1000 # 5 minutes
+
+def test_tokens(page: Page) -> None:
+    page.goto(url)
+    page.get_by_role("button", name="GET /tokens Get Tokens").click()
+    page.get_by_role("button", name="Try it out").click()
+    page.get_by_role("textbox", name="prompt").click()
+    page.get_by_role("textbox", name="prompt").fill(prompt)
+    page.get_by_role("button", name="Execute").click()
+    page.screenshot(path="screenshot-tokens-request.png")
+    page.get_by_role("cell", name="Response body Download { \"").click(timeout=timeout)
+    page.screenshot(path="screenshot-tokens-response.png")
+
+def test_generate(page: Page) -> None:
+    page.goto(url)
+    page.get_by_role("button", name="GET /generate Get Generate").click()
+    page.get_by_role("button", name="Try it out").click()
+    page.get_by_role("textbox", name="prompt").click()
+    page.get_by_role("textbox", name="prompt").fill(prompt)
+    page.get_by_role("button", name="Execute").click()
+    page.screenshot(path="screenshot-generate-request.png")
+    page.get_by_role("cell", name="Response body Download { \"").click(timeout=timeout)
+    page.screenshot(path="screenshot-generate-response.png")
+
+
