cf4b15c9b2d6857ac66d0669ac21b21e0f91be2e
Sam Liu <ontouchstart@gmail.com>
2025-12-12 10:48:23 -0500

    2025/12/12

---
+++ 2025/12/12/README.md
@@ -0,0 +1,9 @@
+# 2025/12/12
+```
+% uv run main.py
+Hello from 12!
+Not in iPython 
+% uv run ipython main.py
+Hello from 12!
+<IPython.terminal.interactiveshell.TerminalInteractiveShell object at 0x10bec1160>
+```
--- 2025/12/12/main.py
+++ 2025/12/12/main.py
@@ -2,5 +2,13 @@ def main():
     print("Hello from 12!")
 
 
+def iPython():
+    try:
+        print(get_ipython())
+    except NameError:
+        print("Not in iPython ")
+
+
 if __name__ == "__main__":
     main()
+    iPython()
