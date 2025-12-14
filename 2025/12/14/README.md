81e3c9219f7c079e73846c14405d3e7e1c5f4e7a
Sam Liu <ontouchstart@gmail.com>
2025-12-14 12:39:47 -0500

    https://ontouchstart.github.io/2025/12/14

---
+++ 2025/12/14/README.md
@@ -0,0 +1,9 @@
+# 2025/12/14 run remote python files (trusted)
+
+[remote-mlx-lm-test_generate](remote-mlx-lm-test_generate)
+
+
+
+[remote-mlx-lm-test_server](remote-mlx-lm-test_server)
+
+
---
+++ 2025/12/14/remote-mlx-lm-test_server.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def run_remote(url):
+    response = requests.get(url)
+    print(f"## [code]({url})")
+    code = response.content.decode("utf-8")
+    print("```python")
+    print(code)
+    print("```")
+    print("## Result")
+    print("```")
+    exec(code, globals())
+    print("```")
+
+
+url = "https://raw.githubusercontent.com/ml-explore/mlx-lm/refs/heads/main/tests/test_server.py"
+run_remote(url)
