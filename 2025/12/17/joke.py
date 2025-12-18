7653982b021611576f969ee11245218601a58160
Sam Liu <ontouchstart@gmail.com>
2025-12-17 22:18:39 -0500

    ontouchstart.github.io/2025/12/17

---
+++ 2025/12/17/joke.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="http://localhost:8080/v1",  # The address of our mlx_lm.server
+    api_key="not-needed",  # No API key is needed for local inference
+)
+
+
+questions = [
+    "Tell me a joke",
+    "Why is it funny?",
+]
+
+messages = [{"role": "user", "content": content} for content in questions]
+completion = client.chat.completions.create(
+    messages=messages, model="openai/gpt-oss-20b"
+)
+
+for choice in completion.choices:
+    print(choice.message.content)
---
+++ 2025/12/17/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 17!")
+
+
+if __name__ == "__main__":
+    main()
