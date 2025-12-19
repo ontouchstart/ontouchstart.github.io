8229e0bbc99f87083a41180da2dc2cb9b43542fb
Sam Liu <ontouchstart@gmail.com>
2025-12-19 10:29:57 -0500

    https://ontouchstart.github.io/2025/12/19

---
+++ 2025/12/19/README.md
@@ -0,0 +1,15 @@
+# 2025/12/19 Random Ideas
+
+## Human Learning in the Age of Machine Learning
+
+## Literate Programming with Python Modules and Packages
+
+## Markdown as input and output format
+
+## Probalistic and Heuristic Learning
+
+## Branching and Immutability
+
+## Layers, Stacks and Graphs
+
+
---
+++ 2025/12/19/main.py
@@ -0,0 +1,23 @@
+def main():
+    content = """# 2025/12/19 Random Ideas
+
+## Human Learning in the Age of Machine Learning
+
+## Literate Programming with Python Modules and Packages
+
+## Markdown as input and output format
+
+## Probalistic and Heuristic Learning
+
+## Branching and Immutability
+
+## Layers, Stacks and Graphs
+
+
+"""
+    with open("README.md", "w") as file:
+        file.write(content)
+
+
+if __name__ == "__main__":
+    main()
