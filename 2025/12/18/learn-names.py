58f031d5e297c889d9e14d7c618e1723afdfd131
Sam Liu <ontouchstart@gmail.com>
2025-12-26 18:27:18 -0500

    from ask_mlx_lm import ask

--- 2025/12/18/learn-names.py
+++ 2025/12/18/learn-names.py
@@ -1,7 +1,8 @@
 from gist import create_github_gist
-from ask_mlx_lm import mlx_lm
+from ask_mlx_lm import ask
 
-model, tokenizer = mlx_lm.load("openai/gpt-oss-20b")
+model_name = "openai/gpt-oss-20b"
+max_tokens = 1024
 
 questions = [
     "What is your name? What is my name?",
@@ -10,26 +11,14 @@ questions = [
     "How do you know?",
 ]
 
-prompt_cache = mlx_lm.models.cache.make_prompt_cache(model)
 gist_content = "# Learn names"
 
-for content in questions:
-    gist_content += f"\n---\n👤\n---\n{content}\n\n"
-    conversation = [{"role": "user", "content": content}]
-    prompt = tokenizer.apply_chat_template(
-        conversation=conversation,
-        add_generation_prompt=True,
-        reasoning_effort="low",
-    )
-    result = mlx_lm.generate(
-        model=model,
-        tokenizer=tokenizer,
-        prompt=prompt,
-        prompt_cache=prompt_cache,
-    )
-    result = result.replace("<|", "\n\n<|")
-    result = result.replace("|>", "|>\n\n")
-    gist_content += f"\n---\n🤖\n---\n {result}\n\n"
+results = ask(model_name, questions)
+
+
+for index, question in enumerate(questions):
+    gist_content += f"\n---\n👤\n---\n {question}\n\n"
+    gist_content += f"\n---\n🤖\n---\n {results[index]}\n\n"
 
 print(
     f"[learn-names](https://gist.github.com/{create_github_gist('learn-names.md', gist_content)})"
