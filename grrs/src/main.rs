f7921861011ee6e152325c7c77686fb70088b6e5
Sam Liu <ontouchstart@gmail.com>
2025-12-27 13:41:51 -0500

    grrs::find_matches

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -9,24 +9,9 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
-    for line in content.lines() {
-        if line.contains(pattern) {
-            let _ = writeln!(writer, "{}", line);
-        }
-    }
-}
-
 fn main() {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path).expect("could not read file");
 
-    find_matches(&content, &args.pattern, &mut std::io::stdout())
-}
-
-#[test]
-fn find_a_match() {
-    let mut result = Vec::new();
-    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
-    assert_eq!(result, b"lorem ipsum\n");
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout())
 }
