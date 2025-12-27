9d9e0aeaf3c8d2c6b3c504e96bb862b7013f56c3
Sam Liu <ontouchstart@gmail.com>
2025-12-26 23:05:28 -0500

    ask_mlx_lm.review

--- src/ask_mlx_lm/__init__.py
+++ src/ask_mlx_lm/__init__.py
@@ -1,6 +1,7 @@
 import mlx_lm
 import mlx
 from .ask import ask
+from .review import review
 
 
-__all__ = ["__version__", "mlx", "mlx_lm", "ask"]
+__all__ = ["__version__", "mlx", "mlx_lm", "ask", "review"]
---
+++ src/ask_mlx_lm/review.py
@@ -0,0 +1,35 @@
+import mlx_lm
+
+
+def review(model_name, code, max_tokens=1024 * 10):
+    model, tokenizer = mlx_lm.load(model_name)
+
+    prompt_cache = mlx_lm.models.cache.make_prompt_cache(model)
+    results = []
+
+    questions = [
+        f"What programming language is following code?\n{code}",
+        "What feature or features are implemented (specification)?",
+        "How are they implemented (algorithm)?",
+        "Are there any bugs in the code?",
+        "If there are bugs, what are their consequences?",
+        "How can we fix the bugs",
+    ]
+    for content in questions:
+        conversation = [{"role": "user", "content": content}]
+        prompt = tokenizer.apply_chat_template(
+            conversation=conversation,
+            add_generation_prompt=True,
+            reasoning_effort="low",
+        )
+        result = mlx_lm.generate(
+            model=model,
+            tokenizer=tokenizer,
+            prompt=prompt,
+            prompt_cache=prompt_cache,
+            max_tokens=max_tokens,
+        )
+        result = result.replace("<|", "\n\n<|")
+        result = result.replace("|>", "|>\n\n")
+        results.append(result)
+    return results
