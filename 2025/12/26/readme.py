eb7402bab5278f71b9b71597493d4a308d360f65
Sam Liu <ontouchstart@gmail.com>
2025-12-26 21:12:21 -0500

    Adventure in the world of 🐍 and 🦀 with maturin and pytest

--- 2025/12/26/readme.py
+++ 2025/12/26/readme.py
@@ -1,6 +1,5 @@
 readme = """
-# 2025/12/26
-Adventure in the world of python and rust with maturin and pytest
+# 2025/12/26 Adventure in the world of 🐍 and 🦀 with maturin and pytest
 
 - [https://doc.rust-lang.org/stable/book/index.html](https://doc.rust-lang.org/stable/book/index.html)
 - [https://www.maturin.rs](https://www.maturin.rs)
