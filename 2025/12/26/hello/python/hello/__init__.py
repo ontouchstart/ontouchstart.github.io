4bdb3ca83f310af1d5faddad38bd97fba4215697
Sam Liu <ontouchstart@gmail.com>
2025-12-26 20:06:38 -0500

    from hello_maturin import sum_as_string

--- 2025/12/26/hello/python/hello/__init__.py
+++
@@ -1,5 +0,0 @@
-from .hello import *  # noqa: F403
-
-__doc__ = hello.__doc__  # noqa: F405
-if hasattr(hello, "__all__"):  # noqa: F405
-    __all__ = hello.__all__  # noqa: F405
--- 2025/12/26/hello/python/tests/test_all.py
+++
@@ -1,6 +0,0 @@
-import pytest  # noqa: F401
-import hello
-
-
-def test_sum_as_string():
-    assert hello.sum_as_string(1, 1) == "2"
