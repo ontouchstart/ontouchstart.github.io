b80fbe8d9069bd4f911c04db3ffed5ff5e6e85ee
Sam Liu <ontouchstart@gmail.com>
2025-12-27 19:02:12 -0500

    grrs/tests/cli.rs

---
+++ grrs/tests/cli.rs
@@ -0,0 +1,31 @@
+// https://github.com/rust-cli/book/blob/master/src/tutorial/testing/tests/cli.rs
+use assert_cmd::cargo::*; // Import cargo_bin_cmd! macro and methods
+use predicates::prelude::*; // Used for writing assertions
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = cargo_bin_cmd!("grrs");
+
+    cmd.arg("foobar").arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("could not read file"));
+
+    Ok(())
+}
+
+use assert_fs::prelude::*;
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = cargo_bin_cmd!("grrs");
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("A test\nAnother test"));
+
+    Ok(())
+}
