f71f5c0d7bd3a97ca7c7ec5d1812b44659ffbd14
Sam Liu <ontouchstart@gmail.com>
2026-01-02 11:05:33 -0500

    2026/01/02

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import ontouchstart
 
 def main():
     print(ontouchstart.url())
+    print("Hello from ontouchstart-dev!")
+    print("2026/01/02")
 
 
 if __name__ == "__main__":
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
     println!("Hello, world!");
+    println!("2026/01/02");
 }
