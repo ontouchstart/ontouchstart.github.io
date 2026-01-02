2d4b4848a8891081cc83966c32277d81edf424f1
Sam Liu <ontouchstart@gmail.com>
2026-01-02 17:16:14 -0500

    ontouchstart = { git = "https://github.com/ontouchstart/ontouchstart.github.io", rev = "2026_01_02" }

---
+++ 2026/01/02/main.py
@@ -0,0 +1,15 @@
+import ontouchstart
+
+
+def main():
+    print("2026/01/02")
+    print(ontouchstart.url())
+    print("resume")
+    for p in ontouchstart.resume():
+        print(p.text)
+
+if __name__ == "__main__":
+    main()
+
+
+
