be8d63074e3acc6b00458ab44d867f70bf20757f
Sam Liu <ontouchstart@gmail.com>
2026-01-02 16:14:18 -0500

    src/ontouchstart_dev

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import ontouchstart
-
-
 def main():
     print(ontouchstart.url())
     print("2026/01/02")
     print("resume")
     for p in ontouchstart.resume():
         print(p.text)
-
 
 if __name__ == "__main__":
     main()
