cfc668f931e64fb27a49f0db1e639a9ddecddce0
Sam Liu <ontouchstart@gmail.com>
2026-01-08 21:04:52 -0500

    .github/workflows/makefile.yml

--- rust-notebook/src/2026/01/07/src/bin/brown_mm.rs
+++ rust-notebook/src/2026/01/07/src/bin/brown_mm.rs
@@ -1,12 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-   let send_body = "{\"model\": \"mlx-community/Llama-3.2-3B-Instruct-4bit\",\"messages\": [{\"role\": \"user\", \"content\": \"Tell me something about brown M&Ms\"}]}";
+    let send_body = "{\"model\": \"mlx-community/Llama-3.2-3B-Instruct-4bit\",\"messages\": [{\"role\": \"user\", \"content\": \"Tell me something about brown M&Ms\"}]}";
 
-   let body: String = ureq::post("http://localhost:8080/v1/chat/completions")
-    .header("Content-Type", "application/json")
-    .send(send_body)?
-    .body_mut()
-    .read_to_string()?;
+    let body: String = ureq::post("http://localhost:8080/v1/chat/completions")
+        .header("Content-Type", "application/json")
+        .send(send_body)?
+        .body_mut()
+        .read_to_string()?;
     println!("{}", body);
-Ok(())
+    Ok(())
 }
-
