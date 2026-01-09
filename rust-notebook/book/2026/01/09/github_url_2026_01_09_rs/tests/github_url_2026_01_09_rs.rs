b2e1346f3b56116142212a7d10c8147f32c0abce
Sam Liu <ontouchstart@gmail.com>
2026-01-09 09:18:59 -0500

    notebook_2026_01_09

---
+++ rust-notebook/book/2026/01/09/github_url_2026_01_09_rs/tests/github_url_2026_01_09_rs.rs
@@ -0,0 +1,7 @@
+#[test]
+fn github_url_2026_01_09_rs() {
+    assert_eq!(
+        github_url_2026_01_09_rs::url(),
+        String::from("https://github.com/ontouchstart/ontouchstart.github.io/tree/2026_01_09_rs")
+    );
+}
