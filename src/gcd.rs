d41cbc4409dadffaae185c5ff992cc38a110d14b
Sam Liu <ontouchstart@gmail.com>
2026-01-14 14:52:11 -0500

    test gcd::test_gcd_panic - should panic

--- src/gcd.rs
+++ src/gcd.rs
@@ -16,3 +16,9 @@ pub fn gcd(mut n: usize, mut m: usize) -> usize {
 fn test_gcd() {
     assert_eq!(gcd(2 * 3 * 4, 3 * 4 * 5), 3 * 4);
 }
+
+#[test]
+#[should_panic]
+fn test_gcd_panic() {
+    assert_eq!(gcd(0, 0), 1);
+}
