00460da23cb6c06ed537f8855790f79ecf80832a
Sam Liu <ontouchstart@gmail.com>
2026-01-14 12:45:08 -0500

    should pass in ontouchstart_2026_01_14_rs_proptest

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-// should fail in ontouchstart_2026_01_14_rs_proptest but pass here
-mod parse_date_0;
-// mod parse_date_1;
+// should pass in ontouchstart_2026_01_14_rs_proptest
+// mod parse_date_0;
+mod parse_date_1;
 
-// should fail in ontouchstart_2026_01_14_rs_proptest but pass here
-pub use parse_date_0::parse_date;
-// pub use parse_date_1::parse_date;
+// should pass in ontouchstart_2026_01_14_rs_proptest
+// pub use parse_date_0::parse_date;
+pub use parse_date_1::parse_date;
 
 #[cfg(test)]
 mod tests {
