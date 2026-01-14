658b2aab76802bacf90e0385b0932a49186126cf
Sam Liu <ontouchstart@gmail.com>
2026-01-14 17:11:38 -0500

    test_gcd_from_strings.rs

--- tests/test_gcd_from_numbers.rs
+++ tests/test_gcd_from_numbers.rs
@@ -15,9 +15,16 @@ fn test_gcd_from_numbers() {
 
 #[test]
 #[should_panic]
-fn test_gcd_from_numbers_panic() {
+fn test_gcd_from_empty_numbers_panic() {
  let empty_numbers = Vec::<usize>::new();
     gcd_from_numbers(empty_numbers);
+    gcd_from_numbers([0].to_vec());
+    gcd_from_numbers([1, 0].to_vec());
+}
+
+#[test]
+#[should_panic]
+fn test_gcd_from_0_numbers_panic() {
     gcd_from_numbers([0].to_vec());
     gcd_from_numbers([1, 0].to_vec());
 }
