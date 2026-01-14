b5cea15dc7b080e4152a921221d39de8340abbda
Sam Liu <ontouchstart@gmail.com>
2026-01-14 11:48:11 -0500

    should pass

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-mod parse_date_0;
-// mod parse_date_1;
+// mod parse_date_0;
+mod parse_date_1;
 
-pub use parse_date_0::parse_date;
-// pub use parse_date_1::parse_date;
+// pub use parse_date_0::parse_date;
+pub use parse_date_1::parse_date;
 
 #[cfg(test)]
 mod tests {
