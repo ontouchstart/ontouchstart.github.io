e9b671c34e10b11567640cb41c692c55c50986cf
Sam Liu <ontouchstart@gmail.com>
2026-01-15 06:58:51 -0500

    init

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# ontouchstart_2026_01_14_rs
+# ontouchstart_2026_01_15_rs
 
-<https://github.com/ontouchstart/ontouchstart.github.io/tree/ontouchstart_2026_01_14_rs>
+<https://github.com/ontouchstart/ontouchstart.github.io/tree/ontouchstart_2026_01_15_rs>
 
-<https://github.com/ontouchstart/ontouchstart.github.io/actions?query=branch%3Aontouchstart_2026_01_14_rs+is%3Asuccess>
+<https://github.com/ontouchstart/ontouchstart.github.io/actions?query=branch%3Aontouchstart_2026_01_15_rs>
--- src/lib.rs
+++ src/lib.rs
@@ -2,27 +2,6 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-mod gcd;
-pub use gcd::gcd;
-
-mod gcd_from_numbers;
-pub use gcd_from_numbers::gcd_from_numbers;
-
-mod gcd_from_strings;
-pub use gcd_from_strings::gcd_from_strings;
-
-mod build_vector_1;
-
-mod build_vector_2;
-
-// should pass in ontouchstart_2026_01_14_rs_proptest
-// mod parse_date_0;
-mod parse_date_1;
-
-// should pass in ontouchstart_2026_01_14_rs_proptest
-// pub use parse_date_0::parse_date;
-pub use parse_date_1::parse_date;
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -31,13 +10,5 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn build_vector_1_eq_build_vector_2() {
-        assert_eq!(
-            build_vector_1::build_vector(),
-            build_vector_2::build_vector()
-        );
     }
 }
