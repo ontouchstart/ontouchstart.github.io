495f15778048344fd7442e2f980af19d52c6ed10
Sam Liu <ontouchstart@gmail.com>
2026-09-01 16:17:06 -0400

    const appName = await page.evaluate('navigator.appName');

--- js-runtime/playwright/browser/tests/browser.spec.ts
+++ js-runtime/playwright/browser/tests/browser.spec.ts
@@ -11,11 +11,15 @@ test('has title', async ({ page }) => {
 
 test('get values', async ({ page }) => {
   await page.goto('https://ontouchstart.github.io/js-runtime/browser');
-  console.log(await page.locator('#appname').innerText());
-  console.log(await page.locator('#appcodename').innerText());
-  console.log(await page.locator('#appversion').innerText());
+  console.log('playwright', await page.locator('#appname').innerText());
+  console.log('playwright', await page.locator('#appcodename').innerText());
+  console.log('playwright', await page.locator('#appversion').innerText());
   await page.getByRole('button', { name: 'Get Values' }).click();
-  console.log(await page.locator('#appname').innerText());
-  console.log(await page.locator('#appcodename').innerText());
-  console.log(await page.locator('#appversion').innerText());
+  console.log('playwright', await page.locator('#appname').innerText());
+  console.log('playwright', await page.locator('#appcodename').innerText());
+  console.log('playwright', await page.locator('#appversion').innerText());
+  const appName = await page.evaluate('navigator.appName');
+  const appCodeName = await page.evaluate('navigator.appCodeName');
+  const appVersion = await page.evaluate('navigator.appVersion');
+  console.log('browser', { appName, appCodeName, appVersion });
 });
