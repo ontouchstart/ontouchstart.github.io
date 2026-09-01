da8b27c6e3b430e007ee973625a66657885c784b
Sam Liu <ontouchstart@gmail.com>
2026-09-01 16:34:57 -0400

    https://ontouchstart.github.io/llama.cpp/transcripts/2026/09/01/2026-09-01_20-24-58_conv_f91d564b.yml

--- js-runtime/playwright/browser/tests/browser.spec.ts
+++ js-runtime/playwright/browser/tests/browser.spec.ts
@@ -9,17 +9,31 @@ test('has title', async ({ page }) => {
 
 });
 
+test('get navigator', async ({page }) => {
+  const navData = await page.evaluate(() => {
+    const data = {};
+    for (let key in navigator) {
+      const value = navigator[key];
+      // Only include properties that are not functions and not objects
+      // (to avoid circular references and non-serializable types)
+      if (typeof value !== 'function' && typeof value !== 'object') {
+        data[key] = value;
+      }
+    }
+    return data;
+  });
+
+  console.log(navData);
+});
+
 test('get values', async ({ page }) => {
   await page.goto('https://ontouchstart.github.io/js-runtime/browser');
   console.log('playwright', await page.locator('#appname').innerText());
   console.log('playwright', await page.locator('#appcodename').innerText());
   console.log('playwright', await page.locator('#appversion').innerText());
   await page.getByRole('button', { name: 'Get Values' }).click();
+  console.log('playwright', 'GetValues');
   console.log('playwright', await page.locator('#appname').innerText());
   console.log('playwright', await page.locator('#appcodename').innerText());
   console.log('playwright', await page.locator('#appversion').innerText());
-  const appName = await page.evaluate('navigator.appName');
-  const appCodeName = await page.evaluate('navigator.appCodeName');
-  const appVersion = await page.evaluate('navigator.appVersion');
-  console.log('browser', { appName, appCodeName, appVersion });
 });
