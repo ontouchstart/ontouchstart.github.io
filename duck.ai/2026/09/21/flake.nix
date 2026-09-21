f0c2cf43c604eb744dae9b94face024087f3e015
Sam Liu <ontouchstart@gmail.com>
2026-09-21 10:15:26 -0400

    nix run "github:ontouchstart/ontouchstart.github.io?dir=duck.ai/2026/09/21"

--- duck.ai/2026/09/21/flake.nix
+++ duck.ai/2026/09/21/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   outputs = { self, nixpkgs }: {
-    packages.aarch64-linux.default = nixpkgs.legacyPackages.aarch64-linux.writeShellScriptBin "duck.ai/2026/09/21" "nix flake show && cat -n flake.nix";
+    packages.aarch64-linux.default = nixpkgs.legacyPackages.aarch64-linux.writeShellScriptBin "duck.ai/2026/09/21" "echo 2026/09/21";
   };
 }
