a3b14280ca7c5b4fc4b132016c5bffcc62babadd
Sam Liu <ontouchstart@gmail.com>
2026-09-21 10:21:29 -0400

    du -hs /nix/store/* | sort -nr

--- duck.ai/2026/09/21/flake.nix
+++ duck.ai/2026/09/21/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   outputs = { self, nixpkgs }: {
-    packages.aarch64-linux.default = nixpkgs.legacyPackages.aarch64-linux.writeShellScriptBin "duck.ai/2026/09/21" "echo 2026/09/21";
+    packages.aarch64-linux.default = nixpkgs.legacyPackages.aarch64-linux.writeShellScriptBin "duck.ai/2026/09/21" "echo 2026/09/21 && du -hs /nix/store/* | sort -nr";
   };
 }
